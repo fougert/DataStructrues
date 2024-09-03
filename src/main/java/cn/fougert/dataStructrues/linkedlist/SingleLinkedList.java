f0af4b72c12ea25b578717ae7253c75ca81c6127
fougert <fougert@163.com>
2024-09-03 23:02:41 +0800

    链表todo

---
+++ src/main/java/cn/fougert/dataStructrues/linkedlist/SingleLinkedList.java
@@ -0,0 +1,18 @@
+package cn.fougert.dataStructrues.linkedlist;
+
+/**
+ * @Author luhuan
+ * @Date 2024/9/3
+ * @Description 单链表实现
+ **/
+public class SingleLinkedList {
+}
+
+
+
+
+class HeroNode{
+
+
+}
+
